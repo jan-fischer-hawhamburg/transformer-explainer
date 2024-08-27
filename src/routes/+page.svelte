8da8bda2c53e0427776672a05e6e5c41cb3bd5d0
jan-fischer-hawhamburg <146171030+jan-fischer-hawhamburg@users.noreply.github.com>
2024-08-27 17:27:13 +0200

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -63,7 +63,7 @@
 
 		// Initialize tokenizer
 		//const tokenizer = await AutoTokenizer.from_pretrained('Xenova/gpt2');
-		const tokenizer = await AutoTokenizer.from_pretrained('dbmdz/german-gpt2');
+		const tokenizer = await AutoTokenizer.from_pretrained('https://huggingface.co/dbmdz/german-gpt2/resolve/main/tokenizer.json');
 
 		// Subscribe input change
 		let initialRun = true;
