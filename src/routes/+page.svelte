2932b9e3d4c4c258a79901189149eb19bb6d84dc
jan-fischer-hawhamburg <146171030+jan-fischer-hawhamburg@users.noreply.github.com>
2024-08-28 12:48:06 +0200

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -33,9 +33,9 @@
 	import { fade } from 'svelte/transition';
 	 // import { AutoTokenizer } from '@xenova/transformers';
 	import { ChevronDoubleRightOutline } from 'flowbite-svelte-icons';
-	import { Tokenizer } from "tokenizers";
-	// import { GPT2Tokenizer } from '@xenova/transformers';
-	// import fs from 'fs';
+
+	import { GPT2Tokenizer } from '@xenova/transformers';
+	
 
 	
 	
